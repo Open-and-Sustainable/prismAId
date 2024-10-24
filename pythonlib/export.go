0f95ae89c85f83f0cf540c31194679ee7cc4a6c6
Riccardo Boero <154505067+ricboer0@users.noreply.github.com>
2024-10-24 17:57:25 +0200

    Update export.go

--- pythonlib/export.go
+++ pythonlib/export.go
@@ -7,12 +7,30 @@ package main
 import "C"
 
 import (
-    "github.com/open-and-sustainable/prismaid" // Adjust the import path to match your module name
+    "github.com/open-and-sustainable/prismaid" // Adjust the import path
+    "unsafe"
 )
 
 //export RunReviewPython
-func RunReviewPython() {
-    prismaid.RunReview()
+func RunReviewPython(input *C.char) *C.char {
+    // Convert C string to Go string
+    goInput := C.GoString(input)
+
+    // Call your Go function with the input
+    output, err := prismaid.RunReview(goInput)
+    if err != nil {
+        // Handle the error, return an error message prefixed with "error:"
+        errorMsg := "error:" + err.Error()
+        return C.CString(errorMsg)
+    }
+
+    // Return the output as a C string
+    return C.CString(output)
+}
+
+//export FreeCString
+func FreeCString(str *C.char) {
+    C.free(unsafe.Pointer(str))
 }
 
 func main() {}
